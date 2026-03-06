a0aa6a177fa1f5ee0241cbfa381c17e708f739f4
Diogo Lima Silva <152068715+s7lva@users.noreply.github.com>
2026-03-06 22:49:43 +0000

    Implement file saving for purchase summary
    Added file handling to save purchase summary and fixed output format for total payment.

--- caixa_supermercado.c
+++ caixa_supermercado.c
@@ -10,6 +10,11 @@ int main(){
     int adicionar;
     float desconto;
 
+
+    FILE *file_ptr;
+    file_ptr = fopen("valores da compra.txt", "w"); 
+
+
     printf("indique o preço a pagar por cada produto: ");
     scanf("%f" , &preco_produto);
 
@@ -21,33 +26,43 @@ int main(){
     }
   
 
-    do
-    {
-       printf("deseja inserir mais produtos");
-       scanf("%d" ,&resposta);
-        
-    
-    
+   do {
+       printf("\nDeseja inserir mais produtos? (1-Sim / 0-Não): ");
+       scanf("%d", &resposta);
 
-    } while(resposta ==1 );{
-      printf("indique quantos produtos quer adicionar");
-      scanf("%d" , &adicionar );
+       if (resposta == 1) { // Só entra aqui se o utilizador quiser mesmo adicionar
+           printf("Indique quantos produtos quer adicionar: ");
+           scanf("%d", &adicionar);
+           quantidade_produto = quantidade_produto + adicionar;
+       }
 
-      quantidade_produto = quantidade_produto + adicionar;
-      
-    }
+    } while (resposta == 1);
+
 
 
     total_a_pagar= quantidade_produto * preco_produto;
+
+   if (file_ptr == NULL) {
+       printf("Error opening file!");
+       return 1;
+   }
 
 if (total_a_pagar<100){
     
     desconto= total_a_pagar* 0.50;
     total_a_pagar = total_a_pagar - desconto;
-    printf("O total a pagar com desconto é : %.2f" , total_a_pagar);
+    printf("O total a pagar com desconto é : %.2d€" , total_a_pagar);
 }else{
     printf("Sem desconto");
 }
+
+   fprintf(file_ptr ,"------Resumo da compra------");
+   fprintf(file_ptr ,"\n Quantidade total %d€\n" , quantidade_produto);
+   fprintf(file_ptr , " Total final pago %d€\n" , total_a_pagar);
+
+
+   fclose(file_ptr);
+   printf("\n dados guardados com sucesso no ficheiro \n");
 
 return 0;
 }
