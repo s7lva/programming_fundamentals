e27a2e05ef8a764b7be097ee2dd8b2ee81ca98a4
Diogo Lima Silva <152068715+s7lva@users.noreply.github.com>
2026-03-06 11:08:11 +0000

    Correct input handling and switch cases for conversions
    Fix temperature conversion input and add cases for 'k', 'K', 'f', and 'F'.  Correction some mistakes.

--- temperaturas.c
+++ temperaturas.c
@@ -2,26 +2,34 @@
 
 int main(){
 
-    int conversor;
+    char conversor;
     float celsius;
     float kelvin;
     float farenheit;
 
 
     printf("indique a temperatura em celsius");
-    scanf("%f", celsius);
+    scanf("%f", &celsius);
 
+    printf("Escolha um do seguintes operadores: k,K,f,K");
+    fflush(stdin);
+    scanf("%c", &conversor);
 
     switch (conversor){
-        case 1:
-        kelvin= celsius +273;
+        case 'k':
+        case 'K':
+        kelvin = celsius + 273;
         printf("A temperatura em kelvins é: %.2f\n" , kelvin);
         break;
-        
-        case 2:
+
+        case 'f':
+        case 'F':
         farenheit = (9/5)*celsius +32; 
         printf("A temperatura em farenheit é: %.2f\n" , farenheit);
-        
+        break;
+
+        default:
+        printf("Opcao invalida.");
     }
 
     return 0;
