a021e4a182038d61ff652885f795374a20574c40
Diogo Lima Silva <152068715+s7lva@users.noreply.github.com>
2026-03-06 11:10:18 +0000

    Refactor quadratic equation handling in equação_2_grau.c
    Doing some ifs to specific cases

--- equação_2_grau.c
+++ equação_2_grau.c
@@ -8,7 +8,7 @@ float b;
 float c;
 float equacao2grauincompleta , equacao2graucompleta , x1,x2;
 float delta;
-
+   
 printf("indique o valor de a");
 scanf("%f" , &a);
 
@@ -18,7 +18,28 @@ scanf("%f" , &b);
 printf("indique o valor de c");
 scanf("%f" , &c);
 
-delta = (b * b) - (4 * a * c);
+
+if(a==0){
+     printf("deixou de ser uma equação de 2 grau");
+}else if (b==0 && a==1){
+     x1 = sqrt(c);
+     x2 = -x1;
+     printf("O x1 da: %.2f e o x2 da %.2f " , x1 ,x2);
+}else if(b==0){
+     x1 = sqrt(-c/a);
+     x2 = -x1;
+     printf("O x1 da: %.2f e o x2 da %.2f " , x1 ,x2);  
+}else if (c==0 && a==1){
+    x1= sqrt(a);
+    x2 = -x1;
+    printf("O x1 da: %.2f e o x2 da %.2f " , x1 ,x2);  
+}else if(c==0){
+    x1= sqrt(-b/a);
+    x2 = -x1;
+    printf("O x1 da: %.2f e o x2 da %.2f " , x1 ,x2);  
+}else{
+
+delta = pow(b , 2) - (4 * a * c);
 if(delta >0){
   float raizDelta = sqrt(delta);
    x1 =(-b +sqrt(delta)) / (2*a);
@@ -29,11 +50,11 @@ if(delta >0){
      x1=0;
      x2 = -b/2*a;
      printf("Os valores de x1=0 e de x2 é : %.2f\n ",x2);
-}else{
+    }else{
 
-    printf("delta negativo erro matematico");
+        printf("delta negativo erro matematico");
+    }
 }
-
 return 0;
 
 }
